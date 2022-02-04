cbb72e4c16cfc4d74d04e90033b70133c0ea5ff4
washington-wj <washingtonj047@gmail.com>
2022-02-04 20:39:39 -0300

    feat: router user manage post, get, put, delete

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,9 +1,12 @@
 const UserController = require('../app/controller/UserController')
 const createValidation = require('../app/validation/user/create')
+const AuthController = require('../app/controller/AuthController')
 
 module.exports = (server, routes, prefix = '/api/v1') => {
   routes
+    .post('/authenticate', AuthController.create)
     .post('/user', createValidation, UserController.create)
     .get('/user', UserController.findAll)
+    .get('/user/:id', UserController.getById)
   server.use(prefix, routes)
 }
