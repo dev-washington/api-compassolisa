c18ab7a132860fa0adff7fb9323eb238e7fa8791
washington-wj <washingtonj047@gmail.com>
2022-02-04 20:38:14 -0300

    feat: route post, get, put and delete

--- src/routes/CarRouter.js
+++ src/routes/CarRouter.js
@@ -5,5 +5,7 @@ module.exports = (server, routes, prefix = '/api/v1') => {
   routes
     .post('/car', CarController.create)
     .get('/car', CarController.getAll)
+    .put('/car', CarController.update)
+    .delete('/car', CarController.delete)
   server.use(prefix, routes)
 }
