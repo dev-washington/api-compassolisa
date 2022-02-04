1142f8a59f79e76c5d3ba7410e73b9a4dffaa848
washington-wj <washingtonj047@gmail.com>
2022-02-04 09:41:19 -0300

    feat: method getAll car

--- src/app/controller/carController.js
+++ src/app/controller/carController.js
@@ -10,6 +10,23 @@ class CarController {
       return res.status(500).json(error.mensage)
     }
   }
+
+  async getAll (req, res, next) {
+    const payload = req.params
+    try {
+      const result = await CarService.findAll({
+        modelo: payload.modelo,
+        cor: payload.cor,
+        ano: payload.ano,
+        acessorios: payload.acessorios,
+        quantidadePassageiros: payload.quantidadePassageiros
+      })
+      return res.status(200).json(result)
+    } catch (error) {
+      next(error)
+      return res.status(500).json(error.mesage)
+    }
+  }
 }
 
 module.exports = new CarController()
