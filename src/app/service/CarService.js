05b2e5dd358f951029b6d6f62a515e4165d6a0df
washington-wj <washingtonj047@gmail.com>
2022-02-04 12:14:16 -0300

    feat: manage rules update car

--- src/app/service/CarService.js
+++ src/app/service/CarService.js
@@ -22,8 +22,8 @@ class CarService {
   }
 
   async update (id, body) {
-    const carUp = await CarRepository.update(id, new CarRepository(body.modelo, body.cor, body.ano, body.acessorios, body.quantidadePassageiros))
-    const result = carUp.value
+    const Upcar = await CarRepository.update(id, new CarRepository(body.modelo, body.cor, body.ano, body.acessorios, body.quantidadePassageiros))
+    const result = Upcar.value
     return new CarRepository(result.modelo, result.cor, result.ano, result.acessorios, result.quantidadePassageiros)
   }
 }
