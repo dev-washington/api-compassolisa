3b6ebd5886f90f64cdfb23370ec688def395d6aa
washington-wj <washingtonj047@gmail.com>
2022-02-04 09:44:15 -0300

    feat: manage rules servcie car

--- src/app/service/carService.js
+++ src/app/service/carService.js
@@ -5,6 +5,15 @@ class CarService {
     const result = await CarRepository.create(payload)
     return result
   }
+
+  async findAll (payload) {
+    const carList = await CarRepository.findAll(payload)
+    carList.map((car) => {
+      return new CarRepository(car.modelo, car.cor, car.ano, car.acessorios, car.quantidadePassageiros)
+    })
+
+    return carList
+  }
 }
 
 module.exports = new CarService()
