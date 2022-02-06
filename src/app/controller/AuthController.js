6b65e11b1be308757f1d3f64f7df34167c1d0fc8
washington-wj <washingtonj047@gmail.com>
2022-02-06 17:53:24 -0300

    feat: autentication user

--- src/app/controller/AuthController.js
+++ src/app/controller/AuthController.js
@@ -4,7 +4,7 @@ class AuthController {
   async create (req, res) {
     try {
       const user = await UserSchema.create(req.body)
-      return res.send({ user })
+      return res.status(200).send(user)
     } catch (error) {
       return res.status(400).send({ error: 'Registration failed' })
     }
