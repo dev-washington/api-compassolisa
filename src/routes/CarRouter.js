b2b5d45ee2df0b8abaa7ec5f46e02efd5f03089f
washington-wj <washingtonj047@gmail.com>
2022-02-06 17:37:10 -0300

    feat: manage post, get, put and delete users

--- src/routes/CarRouter.js
+++ src/routes/CarRouter.js
@@ -1,11 +1,11 @@
 const CarController = require('../app/controller/CarController')
-// const createValidation = require('../app/validation/car/create')
+const CheckCreate = require('../app/validation/car/create')
 
 module.exports = (server, routes, prefix = '/api/v1') => {
   routes
-    .post('/car', CarController.create)
+    .post('/car', CheckCreate, CarController.create)
     .get('/car', CarController.list)
-    .get('/car/:id', CarController.findOne)
+    .get('/car/:id', CarController.findById)
     .put('/car/:id', CarController.update)
     .delete('/car/:id', CarController.delete)
   server.use(prefix, routes)
