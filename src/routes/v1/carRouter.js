44aa0e6ef17bec3db4ece1647180d35bedf50dc2
Washington Jose <75760299+washington-wj@users.noreply.github.com>
2022-02-07 13:49:35 -0300

    Delete carRouter

--- src/routes/v1/carRouter.js
+++
@@ -1,7 +0,0 @@
-const carController = require('../../app/controller/carController')
-
-module.exports = (server, routes, prefix = '/car') => {
-  routes
-    .post('/car', carController.create)
-  server.use(prefix, routes)
-}
