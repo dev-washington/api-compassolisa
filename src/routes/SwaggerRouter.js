97f0bda60c5a0aa25e53c55b5912fff746c3edc8
washington-wj <washingtonj047@gmail.com>
2022-02-14 02:29:32 -0300

    feat: created route swagger

---
+++ src/routes/SwaggerRouter.js
@@ -0,0 +1,10 @@
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocs = require('../../swagger.json')
+
+module.exports = (server, prefix = '/api-docs') => {
+    server.use(
+        prefix,
+        swaggerUI.serve, 
+        swaggerUI.setup(swaggerDocs)
+        )
+  }
