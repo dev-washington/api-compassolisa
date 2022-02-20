631e0a7eddc20f6c7465e3c8ea7d100e2782351d
washington-wj <washingtonj047@gmail.com>
2022-02-20 20:12:17 -0300

    fix all eslint

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
-require('dotenv/config')
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 class Database {
-  constructor () {
-    this.connect()
+  constructor() {
+    this.connect();
   }
 
-  connect () {
-    return mongoose.connect(process.env.MONGODB_LOCAL)
+  connect() {
+    return mongoose.connect(process.env.MONGODB_LOCAL);
   }
 }
 
-module.exports = new Database().connect()
+module.exports = new Database().connect();
