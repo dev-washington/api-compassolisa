a95c171bca5d51503327ebac86a6e7a4d71f652f
washington-wj <washingtonj047@gmail.com>
2022-02-20 20:10:48 -0300

    fix all eslint

--- src/app/service/CarService.js
+++ src/app/service/CarService.js
@@ -1,36 +1,36 @@
-const CarRepository = require('../repository/CarRepository')
+const CarRepository = require('../repository/CarRepository');
 
 class CarService {
-  async create (payload) {
-    const result = await CarRepository.create(payload)
-    return result
+  async create(payload) {
+    const result = await CarRepository.create(payload);
+    return result;
   }
 
-  async findAll (payload) {
-    const result = await CarRepository.findAll(payload)
-    return result
+  async findAll(payload) {
+    const result = await CarRepository.findAll(payload);
+    return result;
   }
 
-  async findById (id) {
-    const result = await CarRepository.findById(id)
-    return result
+  async findById(id) {
+    const result = await CarRepository.findById(id);
+    return result;
   }
 
-  async queryParams (payload) {
-    const result = await CarRepository.queryParams(payload)
-    return result
+  async queryParams(payload) {
+    const result = await CarRepository.queryParams(payload);
+    return result;
   }
 
-  async update (id, payload) {
-    const result = await CarRepository.update(id, payload)
-    return result
+  async update(id, payload) {
+    const result = await CarRepository.update(id, payload);
+    return result;
   }
 
-  async delete (id) {
-    const result = await this.findById(id)
-    await CarRepository.delete(result)
-    return result
+  async delete(id) {
+    const result = await this.findById(id);
+    await CarRepository.delete(result);
+    return result;
   }
 }
 
-module.exports = new CarService()
+module.exports = new CarService();
--- src/app/service/RentalService.js
+++ src/app/service/RentalService.js
@@ -1,31 +1,30 @@
-const RentalRepository = require('../repository/RentalRepository')
+const RentalRepository = require('../repository/RentalRepository');
 
-class RentalRepository{
-    async create (payload){
-        const result = await RentalRepository.create(payload)
-        return result
-    }
+class RentalService {
+  async create(payload) {
+    const result = await RentalRepository.create(payload);
+    return result;
+  }
 
-    async findAll (payload){
-        const result = await RentalRepository.findAll(payload)
-        return result
-        
-    }
+  async findAll(payload) {
+    const result = await RentalRepository.findAll(payload);
+    return result;
+  }
 
-    async findById (id){
-        const result = await RentalRepository.findById(id)
-        return result
-    }
+  async findById(id) {
+    const result = await RentalRepository.findById(id);
+    return result;
+  }
 
-    async update (id, payload){
-        const result = await RentalRepository.update(id, payload)
-        return result
-    }
+  async update(id, payload) {
+    const result = await RentalRepository.update(id, payload);
+    return result;
+  }
 
-    async delete(id){
-        const result = await RentalRepository.delete(id)
-        return result
-    }
+  async delete(id) {
+    const result = await RentalRepository.delete(id);
+    return result;
+  }
 }
 
-modelo.exports = RentalRepository
+module.exports = RentalService;
