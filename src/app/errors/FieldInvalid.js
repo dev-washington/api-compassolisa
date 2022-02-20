eda6c29ccb1d151fd88db5d1d631e54107c12d78
washington-wj <washingtonj047@gmail.com>
2022-02-20 20:08:51 -0300

    fix all eslint

--- src/app/errors/FieldInvalid.js
+++ src/app/errors/FieldInvalid.js
@@ -1,9 +1,9 @@
 class FieldInvalid extends Error {
-  constructor (campo) {
-    const mensagem = `The field ${campo} is invalid`
-    super(mensagem)
-    this.name = 'Invalid Field'
+  constructor(campo) {
+    const mensagem = `The field ${campo} is invalid`;
+    super(mensagem);
+    this.name = 'Invalid Field';
   }
 }
 
-module.exports = FieldInvalid
+module.exports = FieldInvalid;
--- src/app/errors/NotFound.js
+++ src/app/errors/NotFound.js
@@ -1,8 +1,8 @@
 class NotFound extends Error {
-  constructor () {
-    super('Carro não foi encontrado')
-    this.name = 'NotFound'
+  constructor() {
+    super('Carro não foi encontrado');
+    this.name = 'NotFound';
   }
 }
 
-module.exports = NotFound
+module.exports = NotFound;
