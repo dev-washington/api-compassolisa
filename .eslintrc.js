34051f40a6a6bdb9de842b9e1123e2c668cd3036
washington-wj <washingtonj047@gmail.com>
2022-02-20 20:19:57 -0300

    feat: fix all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
   env: {
+    commonjs: true,
     es2021: true,
     node: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: 'latest'
   },
   rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'new-cap': 1,
+    'no-console': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }]
   }
-}
+};
