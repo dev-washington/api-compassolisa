5d3393db616d9d292c4a00601df1976fc49994d0
washington-wj <washingtonj047@gmail.com>
2022-02-20 23:43:20 -0300

    fix all eslint

--- src/app/repository/RentalRepository.js
+++ src/app/repository/RentalRepository.js
@@ -5,9 +5,28 @@ class RentalRepository {
     return RentalSchema.create(payload);
   }
 
-  async findAlll(payload) {
-    return RentalSchema.findAll(payload);
+  async findAll(payload) { 
+     const myCustomLabels = {
+      totalDocs: 'total',
+      docs: 'veiculos',
+      page: 'offset',
+      nextPage: false,
+      prevPage: false,
+      totalPages: 'offsets',
+      pagingCounter: false,
+      meta: false,
+      hasPrevPage: false,
+      hasNextPage: false
+    };
+
+    const options = {
+      page: 1,
+      limit: 100,
+      customLabels: myCustomLabels
+    };
+    return RentalSchema.paginate(payload, options, {});
   }
+  
 
   async findById(id) {
     return RentalSchema.findById({
